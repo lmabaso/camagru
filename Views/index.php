c32b29f044d889ad316d897a8f7cc613d17a1087
Liberty <liberty.mabaso@yahoo.com>
2018-10-19 17:58:07 +0200

    auto commit

---
+++ Views/index.php
@@ -0,0 +1,23 @@
+<?php 
+include_once 'header.php';
+if (Session::exists('success')){
+    echo Session::flash('success');
+}
+if (Session::exists('fail')){
+    echo Session::flash('fail');
+}
+echo '<section class="main-container">
+        <div class="main-wrapper">
+            <h2>Home</h2>';
+if ($user->isLoggedIn()) {
+    echo "<a href='createpost.php'>create post</a>";
+}
+else {
+    echo "<p> Please Login or register to procced";
+}
+?>
+    </div>
+</section>
+<?php
+include_once 'footer.php';
+?>
