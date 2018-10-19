ee167281da6febfee2eb56f629c09ff462797d40
liberty <liberty.mabaso@yahoo.com>
2018-10-19 09:42:04 +0200

    auto commit

--- header.php
+++ header.php
@@ -1,56 +1,36 @@
 <?php
+require_once 'Control/Core/init.php';
 
+$user = new User();
 echo '<!DOCTYPE html>
-	<html>
-	<head>
-	<title>Camagru</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	<body>
-	<header>
-		<nav>
-			<div class="main-wrapper">
-				<ul>
-					<li><a href="index.php">Home </a></li>
-				</ul>
-				<ul>
-					<li><a href="game.php">Game </a></li>
-				</ul>';
-if (isset($_SESSION['u_name']))
+	  <html>
+		<head>
+			<title>Camagru</title>
+			<link rel="stylesheet" type="text/css" href="style.css">
+		</head>
+		<body>
+			<header>
+				<nav>
+					<div class="main-wrapper">
+						<ul>
+							<li><a href="index.php">Home </a></li>
+						</ul>';
+if ($user->isLoggedIn())
 {
-	echo	'<ul>
-			<li><a href="shop.php">Shop</a></li>
-			</ul>
-			<div class="nav-login">';
-	if ($_SESSION['u_name'] == "admin")
-		echo 	'<ul>
-					<li><a href="admin.php">Admin</a></li>
-				</ul>';
-	echo	'<div class="active">Cart - Items : ';
-	echo	count($_SESSION["shopping_cart"]);
-	echo	'</div>';
-	echo	'<div class="active">Total - R ';
-	echo	$_SESSION['total'];
-	echo	'</div>';
-	echo	'<div class="active">Logged in as :  ';
-	echo	$_SESSION["u_name"];
-	echo	'</div>';
-	echo    '<form action="resource/logout.php" method="POST">
-			<button name="submit">Logout</button>
-			</form>';
+	echo '<div class="nav-login">';
+	echo '<a href="profile.php">' .escape($user->data()->user_name) . '</a>';
+	echo '<form action="logout.php" method="POST">
+		 	<button name="submit">Logout</button>
+		  </form>';
 }
 else
 {
-
-	echo '<div class="nav-login"><form action="resource/login.php" method="POST">
-			<input type="text" name="username" placeholder="Username/e-mail">
-			<input type="password" name="pwd" placeholder="password">
-			<button name="submit">Login</button>
-			</form>';
+echo '<div class="nav-login">';
+echo '<a href="login.php">Login</a>';
+echo '<a href="signup.php">Sign up</a>';
 }
-echo '<a href="signup.php">Sign up</a>
-</div>
-</div>
+echo '</div>
+	</div>
 </nav>
 </header>';
 ?>
